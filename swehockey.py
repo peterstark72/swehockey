581c074f125bf644cfca70e723778a6d7cca08c2
Peter Stark <peter.stark@menmo.se>
2014-01-05 15:15:24 +0100

    xmas version

--- swehockey.py
+++ swehockey.py
@@ -2,190 +2,262 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
-from bs4 import BeautifulSoup
-
+import datetime
 from collections import namedtuple
+
+from bs4 import BeautifulSoup
 
 
 PLAYERSBYTEAM_URL = "http://stats.swehockey.se/Teams/Info/PlayersByTeam/{}"
 ROSTER_URL = "http://stats.swehockey.se/Teams/Info/TeamRoster/{}"
 
-
-LEAGUES = {
-    'SHL' : 3905,
-    'HOCKEYALLSVENSKAN' : 3906
-}
-
-
-
-def integer_or_str(v):
-    try:
-        return int(v)
-    except:
-        pass
-    return v
-
-
-def float_or_str(v):
-    try: 
-        return float(v)
-    except:
-        return None
-
-
-def country_code(v):
-    return v[0:3]
-
-
-def strings(td):
-    s = []
-    for st in td.stripped_strings:
-        s.append(st)
-    return u"".join(s)
-
-
-
-
-DATA_MAP = {
-    'country' : country_code, 
-    'sgperc' : float_or_str,
-    'foperc' : float_or_str,
-    'gaa' : float_or_str,
-    'svsperc' : float_or_str
-}
-    
-
-INTEGERS = ('rk', 'no','pos','gp','g', 'a','tp','pim','plus','minus','plusminus','gwg','ppg','shg','sog', 'foplus','fominus','fo', 'weight', 'height', 'gpt', 'gkd', 'gpi', 'ga', 'svs', 'sog', 'so', 'w', 'l')
-for k in INTEGERS:
-    DATA_MAP[k] = integer_or_str
-
-
-def translate(name, value):
-    fnc = DATA_MAP.get(name, None)
-    if fnc:
-        return fnc(value)
-    return value
-
-
 SKATERSSTATS_COLUMNS = ('team', 'rk', 'no','name','pos','gp','g', 'a','tp','pim','plus','minus','plusminus','gwg','ppg','shg','sog', 
 'sgperc','foplus','fominus','fo','foperc')
-
 GOALIESTATS_COLUMNS = ('team', 'rk','no','name','gpt', 'gkd','gpi', 'mip', 'ga', 'svs', 'sog', 'svsperc', 'gaa','so','w','l')
-
 TEAMROSTER_COLUMNS = ('team', 'no', 'name','dob','pos', 'shoots', 'height','weight','country','youthclub')
 
 
+LEAGUE_MAP = {
+    3905 : (u'SHL', '20132014', 'regular'),
+    3906 : (u'Hockeyallsvenskan', '20132014', 'regular'),
+    3928 : (u'Allettan Mellan', '20132014', 'regular'),
+    3929 : (u'Allettan Södra', '20132014', 'regular'),
+    3882 : (u'Division 1 Norra', '20132014', 'regular'),
+    3878 : (u'Division 1 C', '20132014', 'regular'),
+    3930 : (u'Division 1 C Forts.', '20132014', 'regular'),
+    2892 : (u'Elitserien', '20122013', 'regular'),
+    3810 : (u'SM-slutspel', '20122013', 'playoff'),
+    3811 : (u'Kval till Elitserien', '20122013', 'qualification'),
+}
 
-class ShlTable:
 
-    columns = ()
 
-    def load(self):
-        
-        league_id = LEAGUES.get(self.league, None)
+class SweHockeyException(Exception):
+    pass
 
-        if league_id is None:
-            return
 
-        url = self.url.format(league_id)
+
+
+def integer_or_none(v):
+    try:
+        return int(v)
+    except ValueError:
+        pass
+
+def float_or_none(v):
+    try: 
+        return float(v)
+    except ValueError:
+        pass
+
+
+
+INTEGERS = 'rk','no','gp','g','a','tp','pim','plus','minus','plusminus','gwg','ppg','shg','sog','foplus','fominus','fo','weight','height','gpt','gkd','gpi','ga','svs','sog','so','w','l'
+FLOATS= 'sgperc','foperc','gaa','svsperc'
+
+DATA_MAP = {}
+for i in INTEGERS:
+    DATA_MAP[i] = integer_or_none
+for f in FLOATS:
+    DATA_MAP[f] = float_or_none
+
+
+
+
+class RowDataMixin:
+    __slots__ = ()
+
+    @property
+    def splitname(self):
+        '''Returns a tuple of lastame, firstname for a given player'''
+        lastname, firstname = map(lambda x:x.strip().replace("*",""), self.name.split(","))
+        return (lastname, firstname)  
+
+    @property
+    def playerid(self):
+        name = u":".join(self.name).lower()
+        s = u":".join((str(self.no), name))
+        return hash(s)
+
+
+ 
+
+class SkaterStatsRow(RowDataMixin, namedtuple("SkaterStatsRow", SKATERSSTATS_COLUMNS)):
+    __slots__ = ()
+
+
+class GoalieStatsRow(RowDataMixin, namedtuple("GoalieStatsRow", GOALIESTATS_COLUMNS)):
+    __slots__ = ()
+
+    @property
+    def minutes_in_play(self):
+        if ':' in self.mip:
+            minutes, seconds = self.mip.split(':')
+            return datetime.timedelta(minutes=int(minutes), seconds=int(seconds))
+
+
+class TeamRosterRow(RowDataMixin,namedtuple("TeamRosterRow", TEAMROSTER_COLUMNS)):
+    __slots__ = ()  
     
-        html = urllib2.urlopen(url).read()
+    @property
+    def countrycode(self):
+        c = self.country
+        if len(c) >= 3:
+            return c[0:3]  
 
-        self.soup = BeautifulSoup(html)
+    @property            
+    def dateofbirth(self):
+        dt = datetime.datetime.strptime(self.dob, "%Y-%m-%d")
+        return datetime.date(dt.year, dt.month, dt.day)
 
-        return self
 
 
-    def __init__(self, league):
-        self.league = league.upper()
+"""
+
+TABLES
+
+
+"""
+class AbstractShlTable:
+
+    rowdata = None
+
+    def __init__(self, soup):
+        self.soup = soup
 
 
     def get_teamnames(self):
-        return [a['href'].rsplit("#")[-1] for a in self.soup.find(id="headerLinks").find_all("a")] 
-
+        '''Returns tuple of team ID and team NAME from the navigation menu above the tables'''
+        all_anchors = self.soup.find(id="headerLinks").find_all("a")
+        return [(a['href'][1:], a.string) for a in all_anchors ]
 
     def gettablerows(self, table):
+        '''Returns table rows, skipping headers'''
         return table.find_all("tr")[3:]
 
-
     def gettable(self, team):
+        '''Returns team table'''
         return self.soup.find(id=team).next_sibling
 
+    def read(self):
 
-
-    def readrows(self):
-
-        for team in self.get_teamnames():
+        for team_id, team_name in self.get_teamnames():
             
-            team_table = self.gettable(team)
+            team_table = self.gettable(team_id)
             table = team_table.find("table", class_="tblContent")
 
             for tr in self.gettablerows(table):
                 
-                data = [team] + [strings(td) for td in tr.find_all("td")]
-                
-                formatted_data =  [translate(k, v) for k,v in zip(self.columns,data)]
+                s = [team_name] + [u"".join(td.stripped_strings) for td in tr.find_all("td")]                
 
-                yield self.rowdata._make(formatted_data)
-
+                data = [DATA_MAP.get(k,unicode)(v) for k,v in zip(self.colnames,s)]
+                    
+                yield self.rowdata._make(data)
 
 
     def __iter__(self):
-        return self.readrows()
+        return self.read()
 
     def fetch(self):
         return list(self)
 
+    @property
+    def colnames(self):
+        return self.rowdata._fields
 
 
-class SkatersStats(ShlTable):
-    columns = SKATERSSTATS_COLUMNS
-    url = PLAYERSBYTEAM_URL
-    rowdata = namedtuple("Player", SKATERSSTATS_COLUMNS)
+
+class SkatersStatsTable(AbstractShlTable):
+    rowdata = SkaterStatsRow
 
 
-class GoalieStats(ShlTable):
-    columns = GOALIESTATS_COLUMNS
-    url = PLAYERSBYTEAM_URL
-    rowdata = namedtuple("Player", GOALIESTATS_COLUMNS)
+class GoalieStatsTable(AbstractShlTable):
+    rowdata = GoalieStatsRow
 
     def gettable(self, team):
         skaters_table = self.soup.find(id=team)
         goalie_table = skaters_table.find_next_siblings("table")[1]
         return goalie_table
 
+    def gettablerows(self, table):
+        '''Returns table rows, skipping headers'''
+        return table.find_all("tr")[2:]
 
 
-class TeamRosters(ShlTable):
-    columns = TEAMROSTER_COLUMNS
-    url = ROSTER_URL
-    rowdata = namedtuple("Player", TEAMROSTER_COLUMNS)
+class TeamRostersTable(AbstractShlTable):
+    rowdata = TeamRosterRow
 
     def gettablerows(self, table):
         return table.find_all("tr")[3:-2]
 
 
 
-def goalie_stats(league):
-    return GoalieStats(league).load()
+
+def default_loader(url):
+    '''Loads HTML from the given URL'''
+    try:
+        response = urllib2.urlopen(url)
+        content = response.read()
+        return content
+    except:
+        raise SweHockeyException("Could not load {}".format(url))
 
 
-def skater_stats(league):
-    return SkatersStats(league).load()
+
+class SHLPageReader(object):
+    def __init__(self, league_id, loader=default_loader):
+        url = self.url.format(league_id)
+        html = loader(url)
+        self.soup = BeautifulSoup(html)
 
 
-def all_players(league):
-    return TeamRosters(league).load()
+class TeamStatsReader(SHLPageReader):
+    url = PLAYERSBYTEAM_URL
+
+    @property
+    def skaters(self):
+        return SkatersStatsTable(self.soup)
+    
+    @property
+    def goalies(self):
+        return GoalieStatsTable(self.soup)
+
+
+class RostersReader(SHLPageReader):
+    url = ROSTER_URL
+
+    @property
+    def players(self):
+        return TeamRostersTable(self.soup)
+
+
 
 
 def main():
-    for s in all_players('shl'):
-        print s
-    for s in goalie_stats('shl'):
-        print s
-    for s in skater_stats('shl'):
-        print s
+
+    import sys
+    import csv
+    import itertools
     
+    if len(sys.argv) < 2:
+        raise SystemExit("Expected one or many swehockey league ids, e.g. 3905 3906")
+
+    leagues = sys.argv[1:]        
+    writer = csv.writer(sys.stdout)
+
+    def asutf8(v): return unicode(v).encode('utf-8')
+    
+    for league in leagues:
+
+        swe_reader = TeamStatsReader(league)
+
+        skaters = swe_reader.skaters
+        goalies = swe_reader.goalies
+
+                
+        for p in itertools.chain(skaters, goalies):
+            writer.writerow([asutf8(v) for v in list(p)])
+
 
 
 if __name__ == '__main__':
